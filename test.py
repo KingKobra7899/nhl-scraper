eca49d5f260b7e3a95ea5a9929503cebc43fab51
KingKobra7899 <73727379+KingKobra7899@users.noreply.github.com>
2025-11-16 16:47:55 -0500

    cleaning repo

--- test.py
+++
@@ -1,8 +0,0 @@
-import nhl_scraper.games as g
-import nhl_scraper.player as p
-import pandas as pd
-import seaborn as sns
-
-shots = g.scrapeGamesPbp(g.getSeasonPlayedGames(20202021, 20242025))["shots"]
-
-shots.to_csv("current_season.csv")
